69c3bc9c1f5d9bd1a4926d5e8989b345c324098b
Amir <amir.woas@gmail.com>
2023-07-11 15:57:59 +0330

    3 section done

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -18,14 +18,14 @@ const Category = () => {
 
   return (
     <>
-    <div className='w-full flex flex-col items-center justify-center border-none px-8 py-20'>
+    <div className='w-full flex flex-col items-center justify-center border-none  py-20'>
         <h1 className='text-slate-50 font-semibold text-2xl sm:text-3xl pb-20'> Find what moves you</h1>
-        <div className='w-full h-full gap-y-16 lg:gap-x-36 sm:gap-x-24 xl:gap-x-0 flex flex-wrap flex-row-reverse items-center justify-center sm:justify-around pt-20'>
+        <div className='w-full h-full grid gap-x-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:grid-rows-1 sm:grid-rows-2 grid-rows-4 gap-y-10 pt-20'>
             {/* acordian image */}
             {data.map((item, index)=> {
                 return (
-                    <div className='px-10 min-[543px]:px-16 sm:px-2 min-[783px]:px-8 min-[927px]:px-12 min-[1117px]:px-20 xl:px-0'>
-                 <section onClick={() => handleClick(index)} style={{backgroundImage: `url(${select == index ? `${item.bigImg}` : `${item.miniImg}`})`}} className={`bg-cover bg-no-repeat bg-center duration-300 bg-red-500 relative h-96 rounded-tl-[50px] ${ select == index ? "w-80" : " w-40"}`}>
+                    <div className='flex items-center justify-center'>
+                 <section onClick={() => handleClick(index)} style={{backgroundImage: `url(${select == index ? `${item.bigImg}` : `${item.miniImg}`})`}} className={`bg-cover bg-no-repeat bg-center duration-300 bg-red-500 relative h-96 rounded-tl-[50px] ${ select == index ? "w-72 sm:w-80" : " w-40"}`}>
                     {/* fade */}
                     <span className={`w-full h-full duration-300 absolute rounded-tl-[50px] flex items-end justify-center ${ select == index ? "active-fade" : "fade-normal"}`}>
                         <div className='absolute w-full h-1/3 gap-x-4 px-1 text-slate-50 flex items-end justify-center'>
--- src/pages/LanidingPage/index.jsx
+++ src/pages/LanidingPage/index.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import Home from '../../components/Home';
 import Category from '../../components/Category';
+import Instructors from '../../components/Instructors';
+import Download from '../../components/Download';
+import Footer from '../../components/Footer';
 
 const LandingPage = () => {
   return (
     <>
-    <main className='w-full bg-[#18171A] overflow-hidden h-full 2xl:container mx-auto'>
       <Navbar />
       <Home />
-      <Category/>
-    </main>
+      <main className="w-full bg-[#18171A] overflow-hidden h-full 2xl:container mx-auto md:px-10 px-8">
+        <Category />
+        <Instructors />
+      </main>
+      <Download/>
+      <Footer/>
     </>
   );
 };
