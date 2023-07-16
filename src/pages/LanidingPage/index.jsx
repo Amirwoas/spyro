302df3ba8f96e4cb8804b75281bda9710b82635a
Sina Beyki <mrsaher83@gmail.com>
2023-07-16 10:39:28 +0330

    Landing Page Edited.

--- src/pages/LanidingPage/index.jsx
+++ src/pages/LanidingPage/index.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-import Home from '../../components/Home';
-import Category from '../../components/Category';
-import Instructors from '../../components/Instructors';
-import Download from '../../components/Download';
-import Footer from '../../components/Footer';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Home from "../../components/Home";
+import Category from "../../components/Category";
+import Instructors from "../../components/Instructors";
+import Download from "../../components/Download";
+import Footer from "../../components/Footer";
+import FeedBack from "../../components/FeedBack";
+import ClassSec from "../../components/ClassSec/ClassSec";
+import FitInf from "../../components/FitInf";
+import QuestionBox from "../../components/QuestionBox"
 
 const LandingPage = () => {
   return (
     <>
-      <Navbar />
-      <Home />
-      <main className="w-full bg-[#18171A] overflow-hidden h-full 2xl:container mx-auto md:px-10 px-8">
+      <main className='w-full bg-[#18171A] overflow-hidden h-full 2xl:container mx-auto '>
+        <Navbar />
+        <Home />
         <Category />
+        <FitInf />
+        <ClassSec />
         <Instructors />
+        <Download />
+        <FeedBack />
+        <QuestionBox />
+        <Footer />
       </main>
-      <Download />
-      <Footer />
     </>
   );
 };
